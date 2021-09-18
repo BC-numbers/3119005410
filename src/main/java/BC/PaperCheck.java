c2fe4204b5af914a8060c173b1b5a2bc555cc857
unknown <1442867841@qq.com>
2021-09-18 12:47:33 +0800

    测试完成

--- src/main/java/BC/PaperCheck.java
+++ src/main/java/BC/PaperCheck.java
@@ -2,42 +2,46 @@ package BC;
 
 import BC.utils.GetSimiarityUtil;
 import BC.utils.IOUtil;
+import sun.misc.IOUtils;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import java.io.File;
+import java.util.Scanner;
 
 import static BC.utils.IOUtil.textToString;
 
 public class PaperCheck {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
-        System.out.println("hello World");
-        PaperCheck d = new PaperCheck();
-        d.getRepetiveRate("D:/JAVA/IntelliJ IDEA 2021.2.2/3119005410/src/main/file/orig.txt", "D:/JAVA/IntelliJ IDEA 2021.2.2/3119005410src/main/orig_0.8_add.txt");
-        //用命令行输入
+        String originalPath = textToString(args[0]);
+        String plagiarizePath = textToString(args[1]);
+        String resultPath = textToString(args[2]);
 
     }
-
 
     /**
      * @param originalPath   原文件路径
      * @param plagiarizePath 抄袭文件路径
      */
     public void getRepetiveRate(String originalPath, String plagiarizePath) {
+
         File originalFile = new File(originalPath);
         File plagiarizeFile = new File(plagiarizePath);
-        String oriStr = textToString(originalPath); //文本转为字符串
-        String plagStr = textToString(plagiarizePath);
+        String oriStr = IOUtil.textToString(originalPath); //文本转为字符串
+        String plagStr = IOUtil.textToString(plagiarizePath);
         if (!originalFile.exists() || !plagiarizeFile.exists()) {  //文件不存在
             return;
         }
         //余弦相似度计算
-        GetSimiarityUtil getSimiarityUtil = new GetSimiarityUtil(oriStr, plagStr);
+        GetSimiarityUtil getSimiarityUtil = new GetSimiarityUtil(originalPath, plagiarizePath);
         System.out.println("相似度："+getSimiarityUtil.sim());
-        double result = getSimiarityUtil.sim();
-        IOUtil.StringToFile("D:/JAVA/IntelliJ IDEA 2021.2.2/3119005410/src/main/file/result.txt", originalPath + "\n" + plagiarizePath + "\n"
+        double result;
+        result = getSimiarityUtil.sim();
+        IOUtil.StringToFile("D:/JAVA/IntelliJ IDEA 2021.2.2/3119005410/src/main/file/result.txt", oriStr + "\n" + plagStr + "\n"
                 + "相似度 ：" + result);
     }
 
---
+++ src/main/java/java/lang/ArrayIndexOutOfBoundsExceptionImpl.java
@@ -0,0 +1,6 @@
+package java.lang;
+
+public class ArrayIndexOutOfBoundsExceptionImpl extends ArrayIndexOutOfBoundsException {
+    public ArrayIndexOutOfBoundsExceptionImpl() {
+    }
+}
